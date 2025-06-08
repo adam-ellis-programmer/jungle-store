38192b9b0bbc51d0d67d17222b4b55f5f9c7cc60
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-08 20:31:05 +0100

    demo user start

---
+++ frontend/src/demo/DemoUser.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DemoUser = () => {
+  return (
+    <div className='demo-btn-container'>
+      <button type='button' className='demo-btn'>
+        test drive app
+      </button>
+    </div>
+  )
+}
+
+export default DemoUser
--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -9,6 +9,7 @@ import FormContainer from '../components/FormContainer'
 import { useLoginMutation } from '../slices/usersApiSlice'
 import { setCredentials } from '../slices/authSlice'
 import { toast } from 'react-toastify'
+import DemoUser from '../demo/DemoUser'
 
 const LoginScreen = () => {
   const [email, setEmail] = useState('')
@@ -49,48 +50,53 @@ const LoginScreen = () => {
   }
 
   return (
-    <FormContainer>
-      <h1>Sign In</h1>
+    <>
+      <FormContainer>
+        <h1>Sign In</h1>
 
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="my-2" controlId="email">
-          <Form.Label>Email Address</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className='my-2' controlId='email'>
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control
+              type='email'
+              placeholder='Enter email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        <Form.Group className="my-2" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Enter password"
-            value={password}
-            autoComplete="true"
-            onChange={(e) => setPassword(e.target.value)}
-          ></Form.Control>
-        </Form.Group>
+          <Form.Group className='my-2' controlId='password'>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type='password'
+              placeholder='Enter password'
+              value={password}
+              autoComplete='true'
+              onChange={(e) => setPassword(e.target.value)}
+            ></Form.Control>
+          </Form.Group>
 
-        {/* while id loading then disabled  */}
-        <Button disabled={isLoading} type="submit" variant="primary">
-          Sign In
-        </Button>
+          {/* while id loading then disabled  */}
+          <Button disabled={isLoading} type='submit' variant='primary'>
+            Sign In
+          </Button>
 
-        {isLoading && <Loader />}
-      </Form>
+          {isLoading && <Loader />}
+        </Form>
 
-      <Row className="py-3">
-        <Col>
-          New Customer?{' '}
-          <Link to={redirect ? `/register?redirect=${redirect}` : '/register'}>
-            Register
-          </Link>
-        </Col>
-      </Row>
-    </FormContainer>
+        <Row className='py-3'>
+          <Col>
+            New Customer?{' '}
+            <Link
+              to={redirect ? `/register?redirect=${redirect}` : '/register'}
+            >
+              Register
+            </Link>
+          </Col>
+        </Row>
+      </FormContainer>
+      <DemoUser />
+    </>
   )
 }
 
