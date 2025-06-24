aaf3eff3d477370852110a3814fe54dced7ea813
Adam Ellis <adam.ellis.programmer@gmail.com>
2025-06-24 18:48:23 +0100

    custom start and build commands

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -27,8 +27,6 @@ const userSchema = mongoose.Schema(
   }
 )
 
-console.log(crypto.randomUUID())
-
 userSchema.methods.matchPassword = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
